aec669bc2e5ca67063250191bd66816f861b60ff
iacopo <iacoposalmeri@gmail.com>
2026-05-25 15:31:53 +0200

    finale

--- unisci_database.py
+++ unisci_database.py
@@ -1,8 +1,6 @@
 import pandas as pd
-df1 = pd.read_csv('Risultati_Esplorazione_Totale.csv')
-df2 = pd.read_csv('Risultati_Esplorazione_AITCHISON_PCA_HIGH_VARIANCE.csv')
-df1.loc[df1['Technique'] == 'Aitchison_PCA', 'Technique'] = 'Aitchison_PCA (15 Comp)'
-df2['Technique'] = 'Aitchison_PCA (90% Var)'
+df1 = pd.read_csv('Fase1_Benchmark_Riduzioni_pt1.csv')
+df2 = pd.read_excel('Fase1pt2_Benchmark_Riduzioni.xlsx')
 
 df_totale = pd.concat([df1, df2], ignore_index=True)
-df_totale.to_csv('Risultati_Uniti.csv', index=False)
+df_totale.to_csv('Benchmark_totale.csv', index=False)
