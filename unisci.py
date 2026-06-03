e94b6a8f8ac58948dcd0294236e5f0c4942c2226
gio0310 <giorgioschiera96@gmail.com>
2026-06-03 16:45:33 +0200

    modificati nomi consensus

--- unisci.py
+++ unisci.py
@@ -1,6 +1,6 @@
 import pandas as pd
-df1 = pd.read_csv('/home/giosc/Scrivania/CNR/crc/Tirocinio_Bioinformatica/report_giorgio/GridSearch_FeatureSelection.csv')
-df2 = pd.read_csv('/home/giosc/Scrivania/CNR/crc/Tirocinio_Bioinformatica/report_iacopo/GridSearch_FeatureSelection.csv')
+df1 = pd.read_csv('/home/giosc/Scrivania/CNR/crc/Tirocinio_Bioinformatica/report_giorgio/GridSearch_FeatureSelection_clr.csv')
+df2 = pd.read_csv('/home/giosc/Scrivania/CNR/crc/Tirocinio_Bioinformatica/report_iacopo/GridSearch_FeatureSelection_rclr.csv')
 
 df_totale = pd.concat([df1, df2], ignore_index=True)
 df_totale.to_csv('Gridsearch_finale.csv', index=False)
